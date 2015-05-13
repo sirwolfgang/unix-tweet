283da457b32228652afbb6336ee0977e43343d90
Zane Wolfgang Pickett <Zane.Wolfgang.Pickett@Gmail.com>
2015-05-13 01:23:27 +0000

    handle case where user has deleted themselfs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :authentications, dependent: :destroy
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
 
   accepts_nested_attributes_for :authentications
 
@@ -17,7 +17,12 @@ class User < ActiveRecord::Base
     end
 
     twitter_user_id = authentications.find_by(provider: 'twitter').uid
-    twitter_user = client.user(twitter_user_id.to_i)
+    
+    begin
+      twitter_user = client.user(twitter_user_id.to_i)
+    rescue Twitter::Error::NotFound # User has deleted twitter account
+      return destroy
+    end
 
     update(avatar_url: twitter_user.profile_image_url.to_s) unless twitter_user.default_profile_image?
   end
